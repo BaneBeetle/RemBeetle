937e661473ff63cf81392d0d7871215fa46f81f0
Brian <banebeetle@gmail.com>
2025-10-08 00:02:15 -0400

    Test adding isDisabled to mic

--- frontend/assets/main-DDZdil7B.js
+++ frontend/assets/main-DDZdil7B.js
@@ -110795,14 +110795,15 @@ const ActionButtons = reactExports.memo(
     jsxRuntimeExports.jsxs(HStack, {
       gap: 2,
       children: [
-        /*jsxRuntimeExports.jsx(IconButton, {
+        jsxRuntimeExports.jsx(IconButton, {
           bg: _e ? "green.500" : "red.500",
           ...footerStyles.footer.actionButton,
           onClick: _,
+          isDisabled: !0,
           children: _e
             ? jsxRuntimeExports.jsx(BsMicFill, {})
             : jsxRuntimeExports.jsx(BsMicMuteFill, {}),
-        }),*/
+        }),
         jsxRuntimeExports.jsx(IconButton, {
           "aria-label": "Raise hand",
           bg: "yellow.500",
